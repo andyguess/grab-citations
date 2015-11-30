9dba4b7f1a61b255a74132d13c032558e1761093
andyguess <aguess@gmail.com>
2015-11-29 23:27:52 -0500

    function-ize
    let's make this a function

--- grab_citations.R
+++ grab_citations.R
@@ -5,12 +5,14 @@ library(gdata) # trim()
 
 setwd("~/Documents/Columbia/Dissertation/deposit/Chapter2") # set your directory
 tex_file <- readLines("Chapter2.tex", warn = FALSE)
+bib <- readLines("../Master.bib", warn = FALSE) # master BibTeX file
 head(tex_file)
 
 grep("\\cite[[:lower:]]*[[:punct:][:digit:]]+", tex_file) # Which lines have citations?
 length(grep("\\cite[[:lower:]]*[[:punct:][:digit:]]+", tex_file)) # How many lines?
 
-# function to split out multiple citations in the same line
+## Function to split out multiple citations in the same line
+
 split_cite <- function(c) {
   tmp <- strsplit(c, "\\cite", fixed = TRUE)
   for(i in 1:length(tmp[[1]])) {
@@ -21,28 +23,28 @@ split_cite <- function(c) {
   return(tmp[[1]])
 }
 
-# grab lines with citations (assume \cite names don't start with a #):
-cites <- tex_file[grep("\\cite[[:lower:]]*[[:punct:][:digit:]]+", tex_file)]
-
-# run the split_cite() function on each line:
-cites <- as.vector(unlist(sapply(cites, split_cite)))
-
-# take out everything before the citation on each line:
-cites <- gsub("[[:alnum:][:punct:][:blank:]]+\\\\cite[[:lower:]]*[[:punct:][:digit:]]+", "", cites)
-
-# take out everything after the citation on each line:
-cites <- gsub("}[[:alnum:][:punct:][:blank:]]+", "", cites)
-cites <- gsub("[[:alnum:][:punct:][:blank:]]+[{]", "", cites) # cleanup
-
-# split up individual \cite{} commands with multiple citations (separated by commas)
-cites <- unique(trim(unlist(strsplit(cites, ","))))
-
-# take out lines that are not citations (e.g. \ref{})
-#cites <- cites[!1:length(cites) %in% c(38, 40:42)] # edit this line
-
-## Now check master BibTeX file
-
-bib <- readLines("../Master.bib", warn = FALSE)
-
-# index of citation(s) missing from .bib file
-which(!sapply(1:length(cites), function(c) sum(grep(cites[c], bib, fixed = TRUE)) > 0))
+grab_citations <- function(tex, master_bib) {
+  
+  # grab lines with citations (assume \cite names don't start with a #):
+  cites <- tex[grep("\\cite[[:lower:]]*[[:punct:][:digit:]]+", tex)]
+  
+  # run the split_cite() function on each line:
+  cites <- as.vector(unlist(sapply(cites, split_cite)))
+  
+  # take out everything before the citation on each line:
+  cites <- gsub("[[:alnum:][:punct:][:blank:]]+\\\\cite[[:lower:]]*[[:punct:][:digit:]]+", "", cites)
+  
+  # take out everything after the citation on each line:
+  cites <- gsub("}[[:alnum:][:punct:][:blank:]]+", "", cites)
+  cites <- gsub("[[:alnum:][:punct:][:blank:]]+[{]", "", cites) # cleanup
+  
+  # split up individual \cite{} commands with multiple citations (separated by commas):
+  cites <- unique(trim(unlist(strsplit(cites, ","))))
+  
+  # take out lines that are not citations (e.g. \ref{}):
+  #cites <- cites[!1:length(cites) %in% c(38, 40:42)] # edit this line
+  
+  # index of citation(s) missing from .bib file
+  return(which(!sapply(1:length(cites), function(c) sum(grep(cites[c], master_bib, fixed = TRUE)) > 0)))
+  
+}
